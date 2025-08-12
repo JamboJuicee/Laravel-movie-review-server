94c1a19c82e763b39047059f9e019a2fdf0994bb
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-12 14:29:47 +0200

    Added 3 routes to retrieve and post data

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::get('/movies', [MovieController::class, 'getAllMovies']);
+Route::get('/movies/{movie}/reviews', [MovieController::class, 'getReviews']);
+Route::post('/movies/{movie}/reviews', [MovieController::class, 'addReview']);
