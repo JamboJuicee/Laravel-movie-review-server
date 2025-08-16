0423e9860dec8a9072b3bd48ce2941df9ed30c2f
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-16 13:31:05 +0200

    implemented Movie Controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,21 +5,46 @@ namespace App\Http\Controllers;
 use App\Models\Movie;
 use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ThankYou;
 
 class MovieController extends Controller
 {
     public function getAllMovies() {
-        return Movie::all();
+        Log::info("Retrieving all movies");
+        return response()->json(Movie::all());
     }
 
     public function getReviews(Movie $movie) {
-        return $movie->reviews;
+        Log::info("Retrieving all reviews for a movie with id: " . $movie->id);
+        return response()->json($movie->reviews);
     }
 
     public function addReview(Request $request) {
-        $review = new Review($request->all());
-        $review->save();
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|max:255',
+            'message' => 'required|string|min:5|max:255',
+            'rating' => 'required|integer|min:1|max:5',
+            'movie_id' => 'required|exists:movies,id'
+        ]);
 
+        if ($validator->fails()) {
+            Log::warning("Review validation failed");
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $review = Review::create($validator->validated());
+        Log::info("Review saved successfully");
+
+        $movie = Movie::find($request->movie_id);
+        Mail::to($review->email)->send(new ThankYou($movie, $review));
+        Log::info("Email sent");
+        
         return response()->json($review, 201);
     }
 }
